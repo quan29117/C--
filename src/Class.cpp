9be64cb77f2e29808a8aa91c98c9a2e501d52331
quan29117 <dangminhquan29117@gmail.com>
2022-11-09 23:35:56 +0700

    9/11

--- src/Class.cpp
+++ src/Class.cpp
@@ -29,10 +29,11 @@ public:
     double getY()   {return y;}
 };
 
-class Sinh_vien
+class Student
 {
 private:
     std::string full_name;
+    std::string name;
     std::string ID;
     int age;
     double point;
@@ -40,23 +41,23 @@ private:
 
 public:
 //Hàm tạo - Constructor
-    Sinh_vien() = default;  //khi chỉ muốn khởi tạo
+    Student() = default;  //khi chỉ muốn khởi tạo
 
-    Sinh_vien(const std::string &ho_ten, const std::string &id, int tuoi, double diem, bool tot_nghiep = false) 
-    {                                           //định nghĩa
-        full_name = ho_ten;
-        ID = id;
-        age = tuoi;
-        point = diem;
-        graduate = tot_nghiep;
-    }
+    // Student(const std::string &ho_ten, const std::string &id, int tuoi, double diem, bool tot_nghiep = false) 
+    // {                                           //định nghĩa
+    //     full_name = ho_ten;
+    //     ID = id;
+    //     age = tuoi;
+    //     point = diem;
+    //     graduate = tot_nghiep;
+    // }
 
-    Sinh_vien(const std::string &ho_ten, const std::string &id, int tuoi, double diem, bool tot_nghiep = false)
+    Student(const std::string &ho_ten, const std::string &id, int tuoi, double diem, bool tot_nghiep = false)
         : full_name(ho_ten), ID(id), age(tuoi), point(diem), graduate(tot_nghiep)
     {   /*định nghĩa thường dùng hơn*/  }
 
 //Hàm hủy - Destructor
-    ~Sinh_vien()
+    ~Student()
     {
         std::cout<<"Da xoa sinh vien" << full_name;
     }
@@ -68,15 +69,24 @@ public:
     double getPoint()           {return point;}
     bool getGraduate()          {return graduate;}
 
+//hàm bạn - friend: dùng khi chỉ muốn 1 số hàm (thủ tục) có khả năng truy cập vào phần tử private mà không cần setter / getter
+    friend void cut_name (Student* temp);
+
+//"this" pointer: bên trong hàm thành viên, con trỏ this trỏ tới đối tượng hiện đang chạy trong hàm thành viên đó
+    //không dùng chung với friend vì bản chất là đã thay đổi đối tượng rồi
+    void Tot_nghiep()
+    {
+        this ->graduate = true;
+    }
+    void Edit_name(const std::string &new_full_name)
+    {
+        this ->full_name = new_full_name;
+    }
 };
 
 int main()
 {
-    double x1,y1,x2,y2;
-    std::cin >> x1 >> y1 >> x2 >> y2;
-    Vector v1 (x1, y1);
-    Vector v2 (x2, y2);
-    v1.print();
-    Vector *v_temp = v1.add(v2);
-    v_temp ->print();
+    Student *A;
+    A -> Edit_name("Dang Minh Quan");
+    std::cout << A->getFullName();
 }
